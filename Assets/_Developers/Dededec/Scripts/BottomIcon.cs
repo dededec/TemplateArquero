0dc08e6856fdf7533de3d706da7ec950eb35d52a
dededec <daviderenacasado@gmail.com>
2022-04-25 11:51:38 +0200

    input system y tmpro

---
+++ Assets/_Developers/Dededec/Scripts/BottomIcon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TemplateArquero
+{
+    public class BottomIcon : MonoBehaviour
+    {
+        private Image selector;
+
+    }
+}
