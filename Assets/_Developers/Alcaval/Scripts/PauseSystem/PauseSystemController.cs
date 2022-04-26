29f245502b9eb5cf72bbeb2b598aa17d054d88e7
Alejandro Camús <alcaval1999@gmail.com>
2022-04-26 12:58:33 +0200

    ads added jsjs
    soy imbecil

--- Assets/_Developers/Alcaval/Scripts/PauseSystem/PauseSystemController.cs
+++ Assets/_Developers/Alcaval/Scripts/PauseSystem/PauseSystemController.cs
@@ -6,25 +6,49 @@ using UnityEngine.SceneManagement;
 public class PauseSystemController : MonoBehaviour
 {
     [SerializeField] private GameObject _pausedMenu;
+    [SerializeField] private AudioSource _audiosource;
     public void PauseGame()
     {
         _pausedMenu.SetActive(true);
-        Time.timeScale = 0f;
+        GameState newGameState = GameState.Paused;
+        GameStateManager.instance.SetState(newGameState);
+        // Time.timeScale = 0f;
     }
 
     public void ResumeGame()
     {
         _pausedMenu.SetActive(false);
-        Time.timeScale = 1f;
+        GameState newGameState = GameState.Gameplay;
+        GameStateManager.instance.SetState(newGameState);
+        // Time.timeScale = 1f;
     }
 
     public void ReturnToHome()
     {
-
+        //Load main scene
     }
 
     public void ToggleVolume()
     {
-
+        if(_audiosource.volume == 0f) _audiosource.volume = 1f;
+        else _audiosource.volume = 0f;
     }
+
+    #region Example of new way of pausing game in each of the scripts of the things we need to pause or resume
+
+    private void Awake() {
+        GameStateManager.instance.onGameStateChanged += onGameStateChanged;
+    }
+
+    private void OnDestroy() {
+        GameStateManager.instance.onGameStateChanged -= onGameStateChanged;
+    }
+
+    private void onGameStateChanged(GameState newGameState)
+    {
+        enabled = newGameState == GameState.Gameplay;
+        print("uwu");
+    }
+        
+    #endregion
 }
