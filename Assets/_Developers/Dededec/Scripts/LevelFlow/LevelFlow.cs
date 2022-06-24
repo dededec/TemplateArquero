31915ef6c7146c95f44197a51f8c9422613eac83
Alejandro Camús <alcaval1999@gmail.com>
2022-06-24 10:56:39 +0200

    mundo 1

--- Assets/_Developers/Dededec/Scripts/LevelFlow/LevelFlow.cs
+++ Assets/_Developers/Dededec/Scripts/LevelFlow/LevelFlow.cs
@@ -42,7 +42,7 @@ public class LevelFlow : MonoBehaviour
             _accItem.Add(new Reward(item.id, 1));
         }
         
-        _questManager.ProgressQuest("DefeatEnemies");
+        if(_questManager != null) _questManager.ProgressQuest("DefeatEnemies");
         Destroy(enemy.gameObject);
         
         if(_enemies.Count <= 0)
