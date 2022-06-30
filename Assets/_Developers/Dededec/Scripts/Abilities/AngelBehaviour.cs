75650fe738934a2ce14272d37e25de62aa8c6919
dededec <daviderenacasado@gmail.com>
2022-06-30 10:14:57 +0200

    configuracion inicial

--- Assets/_Developers/Dededec/Scripts/Abilities/AngelBehaviour.cs
+++ Assets/_Developers/Dededec/Scripts/Abilities/AngelBehaviour.cs
@@ -5,26 +5,12 @@ using System;
 
 public class AngelBehaviour : MonoBehaviour
 {
-    // [SerializeField] private AbilityManager _playerAbilities;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // _playerAbilities = GameObject.FindGameObjectWithTag("Car").GetComponent<AbilityManager>();
-        // _playerAbilities = AbilityManager.instance;
-        // if(_playerAbilities == null)
-        // {
-        //     Debug.LogError("AbilityManager no encontrado.");
-        // }
-    }
-
     private void OnTriggerEnter(Collider other) 
     {
         if(!enabled) return;
         
-        if(other.gameObject.tag == "Car")
+        if(other.gameObject.tag == "Player")
         {
-            Debug.Log("NUEVA HABILIDAD MAMAHUEVASO");
             AbilityManager.instance.PickNewAbility();
             this.enabled = false;
         }
